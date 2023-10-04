df450b7e491e07d386c378c7345a3afe49c85276
Daniel <>
2023-10-04 08:54:51 +0300

    Added logout button

--- frontend/xperks-fe/src/components/Navbar.jsx
+++ frontend/xperks-fe/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import {LuLogOut} from 'react-icons/lu'
 import Avatar from 'react-avatar';
 
 const Navbar = () => {
@@ -21,6 +22,12 @@ const Navbar = () => {
                 </span>
                 <span className='ml-3'>username</span>
             </li>
+            <li className='p4'>
+                <button className='bg-white text-black rounded-md p-1.5 ml-3 hover:bg-gray-300 flex items-center px-4'>
+                    <LuLogOut/>
+                    <span className='ml-2'>Logout</span>
+                </button>
+            </li>
         </ul>
 
         <div onClick={handleNav} className='block md:hidden'>
@@ -31,11 +38,17 @@ const Navbar = () => {
             <h1 className='text-5xl font-bold text-[#24f7dd] m-4'>xPerks</h1>
             <ul className='p-4'>
                 <li className='p-4 ml-8'>Rate: 1p = 0.025 EGLD</li>
-                <li className='p-4 border-b border-b-gray-500' >
+                <li className='p-4 ' >
                     <span className='pr-5 ml-8'>
                         <Avatar size={45} name="Username" round={true} />
                     </span>
                     <span >username</span>
+                </li>
+                <li className='p-4 border-b border-b-gray-500 pl-20'>
+                    <button className='bg-white text-black rounded-md p-1.5 ml-3 hover:bg-gray-300 flex items-center px-4'>
+                        <LuLogOut/>
+                        <span className='ml-2'>Logout</span>
+                    </button>
                 </li>
                 <li className='p-4 mt-5 uppercase border-b border-b-gray-900'>Profile</li>
                 <li className='p-4 uppercase border-b border-b-gray-900'>Point History</li>
