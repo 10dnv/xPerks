496aac2aa17c7f5f0a14252a9777a6ffb1234d5e
Daniel <>
2023-10-19 04:45:18 +0300

    styled my profile section

--- frontend/xperks-fe/src/pages/Profile.jsx
+++ frontend/xperks-fe/src/pages/Profile.jsx
@@ -47,39 +47,90 @@ const Profile = () => {
     }
 
   return (
-    <div className='text-white h-[80vh] mx-10 w-2/3  rounded-md  shadow-elrond-900 shadow-2xl'>
+    <div className='text-white h-auto mx-10 w-2/3  rounded-md  shadow-elrond-900 shadow-2xl'>
         <NotificationContainer/>
-        <div className='flex flex-col items-center justify-center h-96 '>
-            <h1 className='text-[50px]  p-20 text-[#8D8D8D]'>My Profile</h1>
-                <dl className="grid grid-cols-[repeat(2,auto)] gap-x-5 w-max  gap-y-3 text-sm md:text-xl">
-                    <dt>First name:</dt>
-                    <dd className="text-left">{userData.firstName}</dd>
-
-                    <dt>Last name:</dt>
-                    <dd className="text-left">{userData.lastName}</dd>
-
-                    <dt>Email:</dt>
-                    <dd className="text-left">{userData.emailAddress}</dd>
-
-                    <dt>Birthdate:</dt>
-                    <dd className="text-left">{userData.dateOfBirth}</dd>
-
-                    <dt>Empl. Date:</dt>
-                    <dd className="text-left">{userData.employmentDate}</dd>
-
-                    <dt>Superior:</dt>
-                    <dd className="text-left">{userData.superior?.firstName + " " + userData.superior?.lastName}</dd>
-                    
-                    <dt>MultiversX wallet:</dt>
-                    <dd className="text-left">
-                        <input className='text-black px-2' type='text' ref={inputRef_mvsx_addr} value={inputAddressVal} onChange={inputAddrHandler}></input>
-                    </dd>
-                    <dd className="text-left">
-                        <button className='mt-2 bg-white text-black px-2' onClick={updateAddrHandle}>Update</button>
-                    </dd>
-                </dl>
-        </div>
+        <h1 className='text-[50px]  text-center text-[#8D8D8D]'>My Profile</h1>
+  <div class="relative wrap overflow-hidden p-10 h-full">
+    <div class="border-2-2 absolute border-opacity-20 border-gray-700 h-[80%] border" style={{left: "50%"}}></div>
+        {/* <!-- right timeline --> */}
+    <div class="mb-8 flex justify-between items-center w-full right-timeline">
+      <div class="order-1 w-5/12"></div>
+      <div class="z-20 flex items-center order-1 bg-elrond shadow-xl w-8 h-8 rounded-full">
+        <h1 class="mx-auto font-semibold text-lg text-gray-900">1</h1>
+      </div>
+      <div class="order-1 bg-elrond rounded-lg shadow-xl w-5/12 px-6 py-4">
+        <h3 class="mb-3 font-bold text-gray-800 text-xl">Full Name</h3>
+        <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">{userData.firstName + " " + userData.lastName} </p>
+      </div>
     </div>
+
+    {/* <!-- left timeline --> */}
+    <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+      <div class="border-1 w-5/12"></div>
+      <div class="z-20 flex items-center order-1 bg-elrond shadow-xl w-8 h-8 rounded-full">
+        <h1 class="mx-auto text-gray-800 font-semibold text-lg">2</h1>
+      </div>
+      <div class="order-1 bg-elrond rounded-lg shadow-xl w-5/12 px-6 py-4">
+        <h3 class="mb-3 font-bold text-gray-800 text-xl">Email</h3>
+        <p class="text-sm font-medium leading-snug tracking-wide text-gray-800 text-opacity-100">{userData.emailAddress}</p>
+      </div>
+    </div>
+    
+    {/* <!-- right timeline --> */}
+    <div class="mb-8 flex justify-between items-center w-full right-timeline">
+      <div class="order-1 w-5/12"></div>
+      <div class="z-20 flex items-center order-1 bg-elrond shadow-xl w-8 h-8 rounded-full">
+        <h1 class="mx-auto font-semibold text-lg text-gray-800">3</h1>
+      </div>
+      <div class="order-1 bg-elrond rounded-lg shadow-xl w-5/12 px-6 py-4">
+        <h3 class="mb-3 font-bold text-gray-800 text-xl">Birthdate</h3>
+        <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">{userData.dateOfBirth}</p>
+      </div>
+    </div>
+
+    {/* <!-- left timeline --> */}
+    <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+      <div class="order-1 w-5/12"></div>
+      <div class="z-20 flex items-center order-1 bg-elrond shadow-xl w-8 h-8 rounded-full">
+        <h1 class="mx-auto text-gray-800 font-semibold text-lg">4</h1>
+      </div>
+      <div class="order-1 bg-elrond rounded-lg shadow-xl w-5/12 px-6 py-4">
+        <h3 class="mb-3 font-bold text-gray-800 text-xl">Employment date</h3>
+        <p class="text-sm font-medium leading-snug tracking-wide text-gray-800 text-opacity-100">{userData.employmentDate}</p>
+      </div>
+    </div>
+
+     {/* <!-- right timeline --> */}
+     <div class="mb-8 flex justify-between items-center w-full right-timeline">
+      <div class="order-1 w-5/12"></div>
+      <div class="z-20 flex items-center order-1 bg-elrond shadow-xl w-8 h-8 rounded-full">
+        <h1 class="mx-auto font-semibold text-lg text-gray-800">5</h1>
+      </div>
+      <div class="order-1 bg-elrond rounded-lg shadow-xl w-5/12 px-6 py-4">
+        <h3 class="mb-3 font-bold text-gray-800 text-xl">Superior</h3>
+        <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">{userData.superior?.firstName + " " + userData.superior?.lastName}</p>
+      </div>
+    </div>
+{/* <!-- left timeline --> */}
+<div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+      <div class="order-1 w-5/12"></div>
+      <div class="z-20 flex items-center order-1 bg-elrond shadow-xl w-8 h-8 rounded-full">
+        <h1 class="mx-auto text-gray-800 font-semibold text-lg">6</h1>
+      </div>
+      <div class="order-1 bg-elrond rounded-lg shadow-xl w-5/12 px-6 py-4">
+        <h3 class="mb-3 font-bold text-gray-800 text-xl">MultiversX wallet</h3>
+        <p class="text-sm font-medium leading-snug tracking-wide text-gray-800 text-opacity-100">
+            <div className='flex-col'>
+                <input className='text-black px-1 w-[100%]' type='text' ref={inputRef_mvsx_addr} value={inputAddressVal} onChange={inputAddrHandler}></input>
+                <button className='mt-2 bg-white text-black px-2' onClick={updateAddrHandle}>Update</button>
+            </div>
+        </p>
+      </div>
+    </div>
+
+  </div>
+</div>
+// </div>
   )
 }
 
