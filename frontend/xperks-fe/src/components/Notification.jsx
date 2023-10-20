be754e5575a62e2ddeea6bda34fb8387af0f0bc5
Daniel <>
2023-10-20 11:09:09 +0300

    updated notification handler

--- frontend/xperks-fe/src/components/Notification.jsx
+++ frontend/xperks-fe/src/components/Notification.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 
 function Notification(props) {
   return (
-    <div className="w-full md:w-1/2 lg:w-1/3">
+    <div className="w-full md:w-1/2 lg:w-1/3 pb-7"  >
     <div className="flex flex-col space-y-4">
         <div className="flex justify-between py-2 px-4 bg-elrond rounded-lg">
             <div className="flex items-center space-x-4">
                 <div className="flex flex-col space-y-1 text-gray-800">
                     <span className="font-bold">{props.title}</span>
-                    <span className="text-sm">You've been recognized by <span className="font-bold">{props.sender}</span> with <span className="font-bold">{props.amount}</span> points!</span>
+
+                    {props.type == "RECEIVED"?<span className="text-sm">You've been recognized by <span className="font-bold">{props.sender}</span> with <span className="font-bold">{props.amount}</span> points!</span>:
+                    <span className="text-sm">Your request to send <span className="font-bold">{props.amount}</span> pts. to <span className="font-bold">{props.sender}</span> was <span className="font-bold">{props.status}</span>!</span>}
+                    
                 </div>
             </div>
-            <div className="flex-none   text-stone-600 text-xs md:text-sm">x</div>
+            <div className="flex-none   text-stone-600 text-xs md:text-sm" onClick={()=>props.handle(props.id)}>x</div>
         </div>
         </div>
         </div>
--- frontend/xperks-fe/src/pages/Dashboard.jsx
+++ frontend/xperks-fe/src/pages/Dashboard.jsx
@@ -1,22 +1,87 @@
 import React from 'react'
 import useAuth from '../hooks/useAuth'
 import Notification from '../components/Notification'
+import axios from 'axios'
+import { useEffect, useState} from 'react'
 
 function Dashboard() {
   const {auth} = useAuth()
+  const [sentNotif, setSentNotif] = useState([])
+  const [recvNotif, setRecvNotif] = useState([])
+
+  const fetchData = async () =>{
+    setSentNotif([])
+    setRecvNotif([])
+    await axios.get(`${process.env.REACT_APP_API_URL}/api/notification` , { headers: {"Authorization" : `Bearer ${auth.token}`} })
+    .then(res => {
+        res.data?.map((data,idx) =>(
+          // console.log(data)
+            (data.type == "SENT" ?
+                setSentNotif(sentNotif => [...sentNotif, data]):
+                setRecvNotif(recvNotif => [...recvNotif, data]))
+        ))
+        console.log(sentNotif)
+        console.log(recvNotif)
+    })
+    .catch((error) => {
+    console.log(error)
+    });
+  }
+
+ 
+        
+useEffect(() => {
+   fetchData()
+}, [])
+
+
+const handleReadNotif = async (id) => {
+
+  await axios({
+    method: 'PUT',
+    url: `${process.env.REACT_APP_API_URL}/api/notification/read/${id}`,
+    headers: {
+        Authorization: `Bearer ${auth.token}}`
+    },
+    // params :  {responseType: 'DENY' }
+}).then(res => {
+    console.log(res.data)
+    // NotificationManager.success("Recognition was submitted!", "Success!")
+    fetchData()
+        })
+    .catch((error) => {
+        // NotificationManager.error("Error sending the transaction!", "Error")
+        console.log(error)
+        });
+}
+
 
   return (
   <div className='text-white min-h-[80vh] h-auto mx-10 w-2/3  rounded-md  shadow-elrond-900 shadow-2xl'>
         <div className='flex flex-col items-center  '>
-            <h1 className='text-[50px]  pb-10  text-white font-bold'>Dashboard</h1>
+            <h1 className='text-[50px]  pb-20  text-white font-bold'>Dashboard</h1>
+            <h1 className='text-5xl pb-10 font-bold'> Welcome back, <span className='text-elrond' >{auth.firstName +" "+ auth.lastName}</span>!</h1>
+         
+
+            {
+              sentNotif.map((data,idx)=>(
+                data.unread?
+                <Notification  key={idx} title="Transaction 🔥" type ="SENT" sender={data.transactionModel?.receiver.firstName + " " + data.transactionModel?.receiver.lastName} amount={String(data.transactionModel?.amount).substring(1)} status={data.transactionModel?.status} handle={handleReadNotif} id={data.id}/>
+                :console.log("")
+            ))
+            }
+
+            {
+              recvNotif.map((data,idx)=>(
+                data.unread?
+                <Notification  key={idx} title="New Recognition 🔥" type ="RECEIVED" sender={data.transactionModel?.sender.firstName + " " + data.transactionModel?.sender.lastName} amount={String(data.transactionModel?.amount).substring(1)} status={data.transactionModel?.status} handle={handleReadNotif} id={data.id}/>
+                :console.log("")
+            ))
+            }
+
+          {/* <button onClick={fetchData}>FETCH</button> */}
         </div>
 
-        <div className='mx-5'>
-
-          <h1 className='text-2xl pb-10'> Welcome back, {auth.firstName +" "+ auth.lastName}!</h1>
-
-          <Notification title="New Recognition 🔥" type ="sent" sender="Alin Ludu" amount="50"/>
-        </div>
   </div>
   )
 }
